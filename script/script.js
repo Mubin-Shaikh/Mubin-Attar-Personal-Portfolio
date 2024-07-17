3f4b69ca6b24ac315cd8b391e12d52b3172294e3
Mubin Attar <sk.mubinattar@gmail.com>
2024-07-17 12:26:29 +0530

    Update script.js

--- script/script.js
+++ script/script.js
@@ -1,19 +1,20 @@
-// add class navbarDark on navbar scroll
+// Add class navbarDark on navbar scroll
 const header = document.querySelector('.navbar');
-console.log(header)
 window.onscroll = function() {
     const top = window.scrollY;
-    if(top >=100) {
+    if (top >= 100) {
         header.classList.add('navbarDark');
-    }
-    else {
+    } else {
         header.classList.remove('navbarDark');
     }
 }
-// collapse navbar after click on small devices
-const navLinks = document.querySelectorAll('.nav-item')
-const menuToggle = document.getElementById('navbarSupportedContent')
 
-navLinks.forEach((l) => {
-    l.addEventListener('click', () => { new bootstrap.Collapse(menuToggle).toggle() })
-})
+// Collapse navbar after click on small devices
+const navLinks = document.querySelectorAll('.nav-item');
+const menuToggle = document.getElementById('navbarSupportedContent');
+
+navLinks.forEach((link) => {
+    link.addEventListener('click', () => {
+        new bootstrap.Collapse(menuToggle).toggle();
+    });
+});
